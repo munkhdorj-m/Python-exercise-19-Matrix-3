8061cf897a6fe3aae9eabc125fb6f310f7fa3068
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-12-13 09:02:15 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,36 +1,34 @@
 import pytest
 import inspect
-from assignment import subtract_matrices, sum_main_diagonal, sum_rows_columns
+from assignment import count_negative_numbers, find_row_with_max_sum, find_largest_difference
 
 def check_contains_loop(function):
     source = inspect.getsource(function)
     return 'for' in source or 'while' in source
 
-@pytest.mark.parametrize("matrix1, matrix2, expected", [
-    ([[3, 5, 2], [7, 8, 6], [4, 1, 3]], [[1, 4, 0], [2, 3, 1], [3, 2, 1]], [[2, 1, 2], [5, 5, 5], [1, -1, 2]]),
-    ([[10, 8], [6, 4]], [[5, 3], [3, 2]], [[5, 5], [3, 2]]),
-    ([[12, 7, 9], [6, 5, 3], [4, 8, 10]], [[3, 5, 6], [2, 1, 3], [7, 2, 4]], [[9, 2, 3], [4, 4, 0], [-3, 6, 6]])
+@pytest.mark.parametrize("matrix, expected", [
+    ([[1, -2, 3], [-4, 5, -6], [7, -8, 9]], 4),
+    ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], 0),
+    ([[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]], 9)
 ])
-def test1(matrix1, matrix2, expected):
-    assert subtract_matrices(matrix1, matrix2) == expected
-    assert check_contains_loop(subtract_matrices)
+def test1(matrix, expected):
+    assert count_negative_numbers(matrix) == expected
+    assert check_contains_loop(count_negative_numbers)
 
 @pytest.mark.parametrize("matrix, expected", [
-    ([[4, 5, 6], [7, 8, 9], [1, 2, 3]], 15),
-    ([[1, 0, 0], [0, 1, 0], [0, 0, 1]], 3),
-    ([[3, 6, 9], [8, 5, 2], [1, 7, 4]], 12)
+    ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2),
+    ([[10, 20, 30], [5, 15, 25], [1, 2, 3]], 0),
+    ([[4, 5, 6], [7, 8, 9], [10, 11, 12]], 2)
 ])
 def test2(matrix, expected):
-    assert sum_main_diagonal(matrix) == expected
-    assert check_contains_loop(sum_main_diagonal)
+    assert find_row_with_max_sum(matrix) == expected
+    assert check_contains_loop(find_row_with_max_sum)
 
-@pytest.mark.parametrize("matrix, expected_rows, expected_columns", [
-    ([[2, 4, 6], [1, 3, 5], [7, 9, 11]], [12, 9, 27], [10, 16, 22]),
-    ([[1, 1, 1], [2, 2, 2], [3, 3, 3]], [3, 6, 9], [6, 6, 6]),
-    ([[4, 0, 1], [9, 2, 3], [5, 6, 7]], [5, 14, 18], [18, 8, 11])
+@pytest.mark.parametrize("matrix, expected", [
+    ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 8),
+    ([[10, 20], [30, 40]], 30),
+    ([[3, -5, 9], [7, 1, -2]], 14)
 ])
-def test3(matrix, expected_rows, expected_columns):
-    rows, columns = sum_rows_columns(matrix)
-    assert rows == expected_rows
-    assert columns == expected_columns
-    assert check_contains_loop(sum_rows_columns)
+def test3(matrix, expected):
+    assert find_largest_difference(matrix) == expected
+    assert check_contains_loop(find_largest_difference)
